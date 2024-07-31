3e4063fafef4e53cda5ecb329ac777408077facd
Danendra <dlohanata@gmail.com>
2024-07-31 22:05:16 +0800

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+  // console.log(elementsToRemove);
+  for (let i=array.length-1; i>=0; i--) {
+    for (const element of elementsToRemove) {
+      if (array[i] === element) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  console.log(array);
+  return array;
 };
 
 // Do not edit below this line
