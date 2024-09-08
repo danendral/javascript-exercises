e40ad8a0e05e5991422dc06d49719764981b0eff
Danendra <dlohanata@gmail.com>
2024-09-08 15:28:41 +0800

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  // Invalid number
+  if (num < 0) {
+    return "OOPS"
+  }
 
+  // Initiate the series
+  let fibonacciSeries = [0, 1];
+
+  while (fibonacciSeries.length-1 < num) {
+    fibonacciSeries.push(fibonacciSeries[fibonacciSeries.length-2] + fibonacciSeries[fibonacciSeries.length-1]);
+  } 
+  return fibonacciSeries[num]
 };
 
 // Do not edit below this line
