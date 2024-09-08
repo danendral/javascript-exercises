dd418145a1cd322680e5f276d898e467c7986481
Danendra <dlohanata@gmail.com>
2024-09-08 15:16:12 +0800

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let newString = input.split('').filter(char => /[a-zA-Z0-9]/.test(char)).join('').toLowerCase();
 
+  let reversedString = newString.split('').reverse().join('');
+
+  return newString === reversedString;
 };
 
 // Do not edit below this line
